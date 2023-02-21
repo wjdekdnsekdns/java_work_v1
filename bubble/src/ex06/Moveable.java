b2139f7f4f94b618cdd518e484b5d7a724d6ca13
wjdekdns <ekdns8276@naver.com>
2023-02-21 15:13:27 +0900

    bubble 움직임 추가

---
+++ bubble/src/ex06/Moveable.java
@@ -0,0 +1,11 @@
+package ex06;
+
+public interface Moveable {
+	public abstract void left();
+	public abstract void right();
+	public abstract void up();
+	// default 문법 사용해보기
+	// 후크 매서드 ( 구현부만 있고 로직은 없다)
+	// Moveable 구현하는 클래스에서 필요하다면 재 정의해서 사용해라
+	default public void down() {};
+}
---
+++ bubble/src/ex06/PlayerWay.java
@@ -0,0 +1,7 @@
+package ex06;
+
+// enum 타입을 사용하는 이유 - 가독성 
+//데이터 번주화가 필요하다면 enum 타입을 사용할 수 있다
+public enum PlayerWay {
+	LEFT, RIGHT
+}
