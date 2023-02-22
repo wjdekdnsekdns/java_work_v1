9f481925509b5c8a788402fd38d5f2f76d20c5d9
wjdekdns <ekdns8276@naver.com>
2023-02-22 14:03:44 +0900

    적군추가하기

---
+++ bubble/src/ex09/Bubble.java
@@ -0,0 +1,146 @@
+package ex09;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Bubble extends JLabel implements Moveable {
+
+	// 버블에 위치 상태
+	private int x;
+	private int y;
+
+	// 버블에 움직임 방향 상태
+	private boolean left;
+	private boolean right;
+	private boolean up;
+
+	// 적군을 맞춘 상태 : 0 기본 1. (적을 가둔 물방울)
+	private int state;
+
+	private ImageIcon bubble; // 기본 물방울 이미지
+	private ImageIcon bubbled; // 적을 가둔 물방울 이미지
+	private ImageIcon bomb; // 물방울이 터진 상태 이미지
+
+	private Player player;
+	private BackgroundBubbleService backgroundBubbleService;
+
+	// 버블은 플레이어에 의존하고 있다
+	public Bubble(Player player) {
+		this.player = player;
+		initData();
+		setInitLayout();
+		backgroundBubbleService = new BackgroundBubbleService(this);
+		initThread();
+	}
+
+	private void initData() {
+		bubble = new ImageIcon("images/bubble.png");
+		bubbled = new ImageIcon("images/bubbled.png");
+		bomb = new ImageIcon("images/bomb.png");
+		left = false;
+		right = false;
+		up = false;
+		state = 0;
+	}
+
+	private void setInitLayout() {
+		// 플레이어가 있는 위치에 태어나야 한다
+		x = player.getX();
+		y = player.getY();
+		setIcon(bubble);
+		setSize(50, 50);
+		setLocation(x, y);
+
+	}
+
+	private void initThread() {
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				if (player.getpWay() == PlayerWay.LEFT) {
+					// 왼쪽 방향을 보고있다
+					left();
+				} else {
+					right();
+				}
+
+			}
+		}).start();
+	}
+
+	@Override
+	public void left() {
+
+		left = true;
+		for (int i = 0; i < 400; i++) {
+			x--;
+			setLocation(x, y);
+			System.out.println(backgroundBubbleService.leftWall());
+			if (backgroundBubbleService.leftWall()) {
+				break;
+			}
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		up();
+	}
+
+	@Override
+	public void right() {
+		right = true;
+		for (int i = 0; i < 400; i++) {
+			x++;
+			setLocation(x, y);
+			System.out.println(backgroundBubbleService.righrWall());
+			if (backgroundBubbleService.righrWall()) {
+				break;
+			}
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		up();
+	}
+
+	@Override
+	public void up() {
+		up = true;
+		while (true) {
+			y--;
+			setLocation(x, y);
+			if (backgroundBubbleService.topWall()) {
+				break;
+			}
+			try {
+				Thread.sleep(1);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+		clearBubble();
+	}
+	// 메서드 행위(동사) -> 목적어
+	private void clearBubble() {
+		try {
+			Thread.sleep(3000);
+			setIcon(bomb);
+			// 터진 다음에 0.5초 다음에 그림 지우기
+			Thread.sleep(500);
+			setIcon(null);
+			//this.setVisible(false); <-- 메모리에 남아있고 화면에만 안보이는 상태
+			//repaint();
+		}   catch (InterruptedException e) {
+		}
+		
+	}
+}
---
+++ bubble/src/ex09/EnemyWay.java
@@ -0,0 +1,6 @@
+package ex09;
+
+public enum EnemyWay {
+
+	LEFT, RIGHT;
+}
